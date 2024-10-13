078bb915e341d18a77272587d67a3cc46657c441
PRATEEK K <pratheekkpratheekk@gmail.com>
2024-10-13 12:09:32 +0530

    i added the sum code

--- example.c
+++ example.c
@@ -3,4 +3,10 @@ void main(void)
 {
     printf("hello ,namaskara");
     printf("welcome to all");
+    printf("SUM OF TWO NUMBERS");
+    int a=10,b=39,sum;
+    sum=a+b;
+    printf("sum=%d",sum);
+    
+
 }
